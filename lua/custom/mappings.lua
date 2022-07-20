c79fce74d6f0ab3af17f3c7a25de66d744e386de
jessezzhu <jessezzhu@tencent.com>
2022-07-20 13:24:58 +0800

    null-ls and font setting

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -6,4 +6,24 @@ M.other = {
    },
 }
 
+M.lspconfig = {
+   v = {
+      ["<leader>fr"] = {
+         function()
+            local old_func = vim.go.operatorfunc
+            _G.op_func_formatting = function()
+               local start = vim.api.nvim_buf_get_mark(0, "[")
+               local finish = vim.api.nvim_buf_get_mark(0, "]")
+               vim.lsp.buf.range_formatting({}, start, finish)
+               vim.go.operatorfunc = old_func
+               _G.op_func_formatting = nil
+            end
+            vim.go.operatorfunc = "v:lua.op_func_formatting"
+            vim.api.nvim_feedkeys("g@", "n", false)
+         end,
+         "   lsp range formatting",
+      },
+   },
+}
+
 return M
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -19,4 +19,10 @@ return{
   ["goolord/alpha-nvim"] = {
     disable = false,
   },
+   ["jose-elias-alvarez/null-ls.nvim"] = {
+      after = "nvim-lspconfig",
+      config = function()
+         require "custom.plugins.null-ls"
+      end,
+ }
 }
