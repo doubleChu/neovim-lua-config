97fc629f9be143f33b210378512ea76a4b2ec0a8
jessezzhu <jessezzhu@tencent.com>
2022-07-20 17:46:50 +0800

    formatted

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,28 +1,28 @@
-return{
-  -- 编辑只读文件
-  ["lambdalisue/suda.vim"] = {
-    cmd = "SudaWrite",
-  },
-  ["abecodes/tabout.nvim"] = {
-  config = function()
-    require('tabout').setup {}
-  end,
-     wants = {"nvim-treesitter"},
-     after = {"nvim-cmp"}
-  },
-  ["iamcco/markdown-preview.nvim"] = {
-     run = function()
-        vim.fn["mkdp#util#install"]()
-     end,
-  },
-  -- 开启dashboard
-  ["goolord/alpha-nvim"] = {
-    disable = false,
-  },
+return {
+   -- 编辑只读文件
+   ["lambdalisue/suda.vim"] = {
+      cmd = "SudaWrite",
+   },
+   ["abecodes/tabout.nvim"] = {
+      config = function()
+         require("tabout").setup {}
+      end,
+      wants = { "nvim-treesitter" },
+      after = { "nvim-cmp" },
+   },
+   ["iamcco/markdown-preview.nvim"] = {
+      run = function()
+         vim.fn["mkdp#util#install"]()
+      end,
+   },
+   -- 开启dashboard
+   ["goolord/alpha-nvim"] = {
+      disable = false,
+   },
    ["jose-elias-alvarez/null-ls.nvim"] = {
       after = "nvim-lspconfig",
       config = function()
          require "custom.plugins.null-ls"
       end,
- }
+   },
 }
