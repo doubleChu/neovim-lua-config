de1c374b726b2cc219d36202a7c17c827b3c6057
jessezzhu <jessezzhu@tencent.com>
2022-07-27 10:18:56 +0800

    disable 2 css lsp

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -2,8 +2,17 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers =
-  { "html", "cssls", "cssmodules_ls", "stylelint_lsp", "eslint", "tsserver", "emmet_ls", "denols", "jsonls" }
+local servers = {
+  "html",
+  "cssls",
+  -- "cssmodules_ls",
+  -- "stylelint_lsp",
+  "eslint",
+  "tsserver",
+  "emmet_ls",
+  "denols",
+  "jsonls",
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
